4eb14587e9e901d6f30f3d6d1ab8bb10d7d0afc7
Owen-Black <s2695699@ed.ac.uk>
2026-09-21 13:36:18 +0100

    energies to 2d
    energies should only be 2d

--- seam_carve.cpp
+++ seam_carve.cpp
@@ -7,13 +7,13 @@ int sobel(int pos1, int pos2, int pos3, int neg1, int neg2, int neg3) {
     return abs((pos1 + 2 * pos2 + pos3) - (neg1 + 2 * neg2 + neg3));
 }
 
-int*** energies(int*** image, int width, int height) {
-    int*** energy = (int***)malloc(3 * sizeof(int**));
-    for (int k = 0; k < 3; k++) {
-        energy[k] = (int**)malloc(width * sizeof(int*));
-        for (int i = 0; i < width; i++) {
-            energy[k][i] = (int*)malloc(height * sizeof(int));
-            for (int j = 0; j < height; j++) {
+int** energies(int*** image, int width, int height) {
+    int** energy = (int**)malloc(width * sizeof(int*));
+    for (int i = 0; i < width; i++) {
+        energy[i] = (int*)malloc(height * sizeof(int));
+        for (int j = 0; j < height; j++) {
+            energy[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
                 int top_left    = (i > 0 || j > 0)                  ? image[k][j - 1][i - 1] : 0;
                 int top_mid     = (j > 0)                           ? image[k][j - 1][i] : 0;
                 int top_right   = (i < width - 1 || j > 0)          ? image[k][j - 1][i + 1] : 0;
@@ -23,21 +23,21 @@ int*** energies(int*** image, int width, int height) {
                 int bot_mid     = (j < height - 1)                  ? image[k][j + 1][i] : 0;
                 int bot_right   = (i < width - 1 || j < height - 1) ? image[k][j + 1][i + 1] : 0;
 
-                energy[k][i][j] = sobel(top_left, top_mid, top_right, bot_left, bot_mid, bot_right) +
-                            sobel(top_left, mid_left, bot_left, top_right, mid_right, bot_right);
+                energy[i][j] += sobel(top_left, top_mid, top_right, bot_left, bot_mid, bot_right) +
+                               sobel(top_left, mid_left, bot_left, top_right, mid_right, bot_right);
             }
         }
     }
     return energy;
 }
 
-void fix_energies(int** energies, int width, int height) {
+void fix_energies(int*** energies, int width, int height) {
     // Your code here
     return;
 }
 
-void seam_carve(int** energies, int width, int height) {
-    // Your code here
+void seam_carve(int*** energies, int width, int height) {
+    
     return;
 }
 
